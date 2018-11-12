a4eaadfe0da3c279f15da36a92b1f629d3f93eb0
topickering <topickering@gmail.com>
2018-11-12 16:07:29 +0000

    Commit 1: first return of HTML

--- app.rb
+++ app.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
-  "Hello World"
+  "hello!"
+end
+
+get '/secret' do
+  "Mind your own business"
+end
+
+get '/hero' do
+  "I don't wear a cape"
+end
+
+get '/sport' do
+  "I don't like cricket. I love it"
+end
+
+get '/cat' do
+  "<div style='border: 3px dashed red'>
+    <img src='http://bit.ly/1eze8aE'>
+  <div>"
 end
