4399ccd7792a8bee4e417e99a11e16c6f6e35328
topickering <topickering@gmail.com>
2018-11-13 15:38:42 +0000

    final cat naming commit

--- app.rb
+++ app.rb
@@ -24,8 +24,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
+end
+
+get '/cat-form' do
+  erb(:form)
 end
